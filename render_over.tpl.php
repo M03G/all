3c7995e5c71fed7b0321cf94c35a13fbe050fc4c
M03G <m03gen@gmail.com>
2014-06-02 07:35:40 +0000

    Update render_over.tpl.php

--- render_over.tpl.php
+++ render_over.tpl.php
@@ -1,35 +1,25 @@
 <div style="margin-top:-50px;">
 	<table class="table table-striped table-bordered">
+		<?foreach ($tpl['screens'][$tpl['sc']]['lists'] as $key => $point) {?>		
 		<tr>
-			<td></td>
-			<? foreach ($tpl['area'] as $key_area => $area) {?>
-			<th><?= $area['nameu']?></th>
-			<?}?>
-			<td></td>
-		</tr>
-		<?foreach ($tpl['overview']['bort'] as $key_bort => $bort) {?>
-		<tr>
-			<td><?= $bort['numb']?></td>
-				<?$i = 0?>
-				<?foreach ($bort['templates'] as $key_template => $template) {?>
-					<?if ($template['area'] != $i){?>
-						<?if ($i === 0){?>
-						<td>
-						<?}else{?>
-						</td><td>
-						<?}?>
-					<?}?>
-					<span class="progress" style="margin:0;width:95px;display:inline-block;height:40px;<?= (!isset($template['complete']))? 'background-color:#d2d2d2':'background-color:#999999'?>" title="<?= $template['title']?>">
-						<?if($template['complete']){?>
-							<div class="progress-bar progress-bar-success" style="width: <?= $template['complete']?>%;" title="<?= $template['title']?>. Выполнено на <?= $template['complete']?>%"></div>  
-						<?}?>
-					</span>
-					<?$i = $template['area']?>
+			<td width=300 style='font-size:20px;'><b>Борт <?=$point['pni']?>. Лист <?=$point['lid']?></b><br><?=$point['name']?></td>
+			<td width=1400>
+				<?foreach ($tpl['l'][$point['lid']]['oper'] as $key_oper => $oper) {?>
+				<?if ($oper['idc']){
+					$color = 'success';
+					$colormt = 'success_mt';
+				} else {
+					$color = 'default';
+					$colormt = 'default_mt';
+				}?>
+				<span class="label label-<?= $color?> oper" style="width:<?=$tpl['l'][$point['lid']]['hour'] * $oper['tr']?>%;text-align:center;" title="<?=$oper['num']?> <?=$oper['rab']?> - <?=$oper['tr']?>ч."><h5>&nbsp;</h5></span>
+				<?if($oper['mt']){?>
+				<span class="label label-<?= $colormt?> oper" style="width:<?=$tpl['l'][$point['lid']]['hour'] * $oper['mt']?>%;text-align:center;" title="Межоперационное время - <?=$oper['mt']?>ч."><h5>&nbsp;</h5></span>
 				<?}?>
-				</td>
-				<td>%</td>
-			</tr>
+				<?}?>
+			</td>
+			<td style='font-size:20px;' align=center><b style='font-size:25px;'><?= $tpl['l'][$point['lid']]['compl']?>%</b><br><?= $tpl['l'][$point['lid']]['rem']?>ч.</td>
+		</tr>
 		<?}?>
 	</table>
-	<?print_ra($tpl, false, '600px')?>
 </div>
